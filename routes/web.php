8a26beafbff79fa8d9e0dc9d2ce3d849ea086b82
AjiFattah <ajifattah7@gmail.com>
2021-10-14 09:42:25 +0700

    menyelesaikan video ke 7 Post Model

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        'title' => 'Home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
-        "name" => "Aji fattah adriansyah darajat",
-        "email" => "ajifattah7@gmail.com",
-        "image" => "aji.jpeg"
+        'title' => 'About',
+        'name' => 'Aji fattah',
+        'email' => 'ajifattah7@gmail.com',
+        'image' => 'aji.jpeg'
     ]);
 });
 
+Route::get('/posts', [PostController::class, 'index']);
 
-
-Route::get('/blog', [PostController::class, 'index']);
-Route::get('posts/{slug}', [PostController::class, 'show']);
+// halaman single post
+Route::get('posts/{post:slug}', [PostController::class, 'show']);
