8fc596f15591f3d8a8c4a5576c241c4c1aba47ae
AjiFattah <ajifattah7@gmail.com>
2021-10-14 10:46:35 +0700

    menyelesaikan video ke 8 Post Category dan Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,8 @@
-
 @extends('layouts.main')
 
 @section('container')
     <h2>{{ $post->title }}</h2>
+    <p>By. aji fattah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
   <a href="/posts">Back to Posts</a>
