500a3e3f99cb8756b4076198b5a9a80c0e10541f
AjiFattah <ajifattah7@gmail.com>
2021-10-21 07:56:04 +0700

    menyelesaikan video ke 9  Database Seeder

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,10 +9,18 @@ class Post extends Model
 {
     use HasFactory;
 
+
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
 
-    public function category(){
+    public function category()
+    {
         return $this->belongsTo(Category::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,9 +1,12 @@
 @extends('layouts.main')
 
 @section('container')
-    <h2>{{ $post->title }}</h2>
-    <p>By. aji fattah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
+
+    <h2 class="mb-5">{{ $post->title }}</h2>
+    <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
-  <a href="/posts">Back to Posts</a>
+
+<a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
