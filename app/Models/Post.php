2988932e48979bb84ca7ab81acabb8f009a51e20
AjiFattah <ajifattah7@gmail.com>
2021-10-21 11:11:24 +0700

    menyelesaikan video ke 12 Redesign UI

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,18 +9,15 @@ class Post extends Model
 {
     use HasFactory;
 
-
     // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
+    protected $with = ['category', 'author'];
 
-    public function category()
-    {
+    public function category(){
         return $this->belongsTo(Category::class);
     }
 
-    public function author()
-    {
+    public function author(){
         return $this->belongsTo(User::class, 'user_id');
     }
-
 }
