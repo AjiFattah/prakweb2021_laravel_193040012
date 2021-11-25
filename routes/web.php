7a4bd4d40dbfadf7b373e4c0fd5947b6aac707f0
AjiFattah <ajifattah7@gmail.com>
2021-11-25 20:20:02 +0700

    menyelesaikan video ke 23 Authorization

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,13 @@
 <?php
 
-use App\Models\Post;
-use App\Models\User;
-use App\Models\Category;
 
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\AdminCategoryController;
 use App\Http\Controllers\DashboardPostController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,33 +19,36 @@ use App\Http\Controllers\DashboardPostController;
 |
 */
 
-Route::get('/', function() {
+Route::get('/', function () {
     return view('home', [
-        "title" => "Home",
+        'title' => 'Home',
         'active' => 'home'
     ]);
 });
 
-Route::get('/about', function() {
+Route::get('/about', function () {
     return view('about', [
-        "title" => "About",
+        'title' => 'About',
         'active' => 'about',
-        "name" => "Aji fattah",
-        "email" => "ajifattah7@gmail.com",
-        "image" => "aji.jpeg"
+        'name' => 'Aji fattah',
+        'email' => 'ajifattah7@gmail.com',
+        'image' => 'aji.jpeg'
     ]);
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+
+// halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories', function() {
+Route::get('/categories', function(){
     return view('categories', [
         'title' => 'Post Categories',
         'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
+
 
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
@@ -56,32 +57,11 @@ Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', function() {
+Route::get('/dashboard', function(){
     return view('dashboard.index');
 })->middleware('auth');
 
-Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth'); 
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
-
-
-
-
-
-
-
-// Route::get('/categories/{category:slug}', function(Category $category){
-//     return view('posts', [
-//         'title' => "Post by category : $category->name",
-//         'active' => 'categories',
-//         'posts' => $category->posts->load('category', 'author')
-//     ]);
-// });
-
-// Route::get('/authors/{author:username}', function(User $author) {
-//     return view('posts', [
-//         'title' => "Post By Author : $author->name",
-//         'active' => 'author',
-//         'posts' => $author->posts->load('category', 'author')
-//     ]);
-// }); 
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
